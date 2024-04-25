acf60caf942fe259d5aa8037a6616ae59ba9ab9e
AlbertoVilar <albertovilar1@gmail.com>
2024-04-25 15:30:43 -0300

    Addingno Macth Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+
 
 
 function App() {
---
+++ src/routes/NotFound/index.tsx
@@ -0,0 +1,8 @@
+
+export default function NotFound() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <p>Not found!</p>
+      </main>
+    );
+  }
